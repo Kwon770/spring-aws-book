ece5359efed7b88d264ae39881a7fe23144ba264
Kwon770 <sckwon770@gmail.com>
2020-12-31 13:58:09 +0900

    5.6 네이버 로그인

--- src/main/java/com/kwon770/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kwon770/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,16 +28,31 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes) {
-        return ofGoogle(userNameAttributeName, attributes);
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        } else {
+            return ofGoogle(userNameAttributeName, attributes);
+        }
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName,
-                                            Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attribute) {
+        Map<String, Object> response = (Map<String, Object>) attribute.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
