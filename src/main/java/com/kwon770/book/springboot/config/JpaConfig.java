9bd765352d732e44fedfeec0ffc6d339f563a0e6
Kwon770 <sckwon770@gmail.com>
2020-12-31 15:15:30 +0900

    5.7 기존 테스트에 시큐리티 적용하기

---
+++ src/main/java/com/kwon770/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kwon770.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/test/java/com/kwon770/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kwon770/book/springboot/web/HelloControllerTest.java
@@ -1,25 +1,54 @@
 package com.kwon770.book.springboot.web;
 
+import com.kwon770.book.springboot.config.auth.SecurityConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+//@SpringBootTest()
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                                      classes = SecurityConfig.class)
+            })
 public class HelloControllerTest {
+
+//    @Autowired
+//    private WebApplicationContext context;
+//
+//    private MockMvc mvc;
+
+//    @Before
+//    public void setup() {
+//        mvc = MockMvcBuilders
+//                .webAppContextSetup(context)
+//                .apply(springSecurity())
+//                .build();
+//    }
+
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void test_ReturningHello() throws Exception {
         String hello = "hello";
 
@@ -29,6 +58,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void test_ReturningHelloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
